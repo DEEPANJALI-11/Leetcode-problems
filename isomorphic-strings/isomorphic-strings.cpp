e6c4f4671976a91fe96ce2080eba3289b69a18ad
Deepanjali Singh <113447060+DEEPANJALI-11@users.noreply.github.com>
2024-03-23 22:59:10 +0530

    Time: 11 ms, Memory: 11.7 MB - LeetHub

--- isomorphic-strings/isomorphic-strings.cpp
+++ isomorphic-strings/isomorphic-strings.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int n=s.size(), m=t.size();
+        int n=s.size(),m=t.size();
         if(n!=m)
                 return false;
         map<char,vector<int>>m1,m2;
-        int i;
+        int i,j;
         for(i=0;i<n;i++)
-                m1[s[i]].push_back(i);
+            m1[s[i]].push_back(i);
         for(i=0;i<m;i++)
             m2[t[i]].push_back(i);
-        for(i=0;i<n;i++){
+        for(i=0;i<n;i++)
+        {
             if(m1[s[i]]!=m2[t[i]])
                     return false;
         }
