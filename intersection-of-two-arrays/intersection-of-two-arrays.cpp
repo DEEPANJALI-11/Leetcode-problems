f28921e66015e4c6ffc8a0eb63d78002fa84a74e
Deepanjali Singh <113447060+DEEPANJALI-11@users.noreply.github.com>
2024-03-23 18:51:14 +0530

    Time: 7 ms, Memory: 13.3 MB - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        set<int>s;
-        int i,j;
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-        int n=nums1.size(), m=nums2.size();
-        for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                if(nums1[i]==nums2[j]){
-                    ans.push_back(nums1[i]);
-                }
+        int n=nums1.size(),m=nums2.size();
+        map<int,int>m1,m2;
+        set<int>ans;
+        vector<int>res;
+        for(auto a:nums1)
+                m1[a]++;
+        for(auto a:nums2)
+                m2[a]++;
+        for(int i=0;i<n;i++){
+            if(m2.find(nums1[i])!=m2.end()){
+                ans.insert(nums1[i]);
             }
         }
-         ans.erase(unique(ans.begin(), ans.end()), ans.end());
-        return ans;
-        
+        for(auto a:ans)
+                res.push_back(a);
+        return res;
     }
 };
